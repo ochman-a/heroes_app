556c5ccce3eb5b43cfc0fcae94e18333a532f50c
Alexandre OCHMAN <alexandre.ochman@gmail.com>
2020-11-26 14:39:09 +0100

    Auto stash before merge of "master" and "origin/master"

--- lib/playBook.dart
+++ lib/playBook.dart
@@ -19,7 +19,7 @@ class _PlayBookState extends State<PlayBook> {
     loadChapter();
     return Scaffold(
       appBar: AppBar(
-        title: Text("Jeu"),
+        title: Text("Play Book"),
       ),
     );
   }
